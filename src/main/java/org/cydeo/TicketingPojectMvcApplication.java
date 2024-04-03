96389ef03e9593968869bb044259c28ce8db4bfd
Ferhat <parhat002@gmail.com>
2024-04-03 21:49:29 +0300

    Initial commit

---
+++ src/main/java/org/cydeo/TicketingPojectMvcApplication.java
@@ -0,0 +1,13 @@
+package org.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketingPojectMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketingPojectMvcApplication.class, args);
+	}
+
+}
